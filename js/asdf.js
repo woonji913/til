f01ac957ccd14edec265af2209f42ef1d4fde185
woonji913 <johnnyboy0913@gmail.com>
2019-05-02 17:50:52 +0900

    190502 | js axios

---
+++ js/asdf.js
@@ -0,0 +1,14 @@
+const axios = require("axios")
+const url = "http://13.125.249.144:8080/ssafy/daejeon/2/posts"
+axios.get(url)
+    .then(res => {
+        console.log(res.data.posts)
+    })
+const data = {
+    "post": {
+        "title": "title",
+        "content": "content",
+        "author": "author"
+    }
+}
+axios.post(url, data)
